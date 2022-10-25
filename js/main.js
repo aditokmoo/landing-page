277e777ccf0be645848d314028f715a337bf8fc8
Havock48 <adit.github@gmail.com>
2022-10-25 20:06:36 +0200

    responsiveness and js added

---
+++ js/main.js
@@ -0,0 +1,42 @@
+const openNav = document.querySelector('.bars');
+const closeNav = document.querySelector('#close-nav');
+const mobileNav = document.querySelector('.mobile-nav');
+const items = document.querySelectorAll('.item a');
+const itemText = document.querySelectorAll('.item-text');
+
+// Open Navigation
+openNav.addEventListener('click', () => {
+    mobileNav.classList.add('active');
+})
+
+// Close Navigation
+closeNav.addEventListener('click', () => {
+    mobileNav.classList.remove('active');
+});
+
+// Accordion
+items.forEach((item, x) => {
+    item.addEventListener('click', (e) => {
+        e.preventDefault();
+
+        itemText.forEach((content, j) => {
+            if(x == j) {
+                console.log(x);
+                console.log(j);
+
+                content.classList.toggle('active')
+
+                if(content.classList.contains('active')) {
+                    content.style.maxHeight = content.scrollHeight + 'px';
+
+                } else {
+                    content.style.maxHeight = 0;
+                }
+            } else {
+                content.style.maxHeight = 0;
+                content.classList.remove('active')
+            }
+        }) 
+
+    });
+})
